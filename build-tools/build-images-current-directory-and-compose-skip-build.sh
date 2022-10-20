2567a2427bc7acd313bbd004cd646925873913e1
Kirill Kainov <kainov.dev@gmail.com>
2022-10-20 21:25:08 +0300

    add scripts for deploy

---
+++ build-tools/build-images-current-directory-and-compose-skip-build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function build_basic_images() {
+  JAR_FILE=$1
+  APP_NAME=$2
+
+  docker build -f common-module/build-tools/docker/basic/Dockerfile \
+    --build-arg JAR_FILE=${JAR_FILE} \
+    -t ${APP_NAME}:latest \
+    -t ${APP_NAME}:simple .
+}
+
+# Building the app
+cd ..
+
+APP_VERSION=0.0.1-SNAPSHOT
+
+echo "Building Docker images"
+build_basic_images medical-monitoring/core/target/monitoring-core-${APP_VERSION}.jar application/medical-monitoring
+build_basic_images message-analyzer/core/target/message-analyzer-core-${APP_VERSION}.jar application/message-analyzer
+build_basic_images person-service/core/target/person-service-core-${APP_VERSION}.jar application/person-service
+
+# Docker compose
+cd ./common-module/build-tools/compose/simple
+docker-compose up -d
+cd ..
+cd ..
+cd ..
